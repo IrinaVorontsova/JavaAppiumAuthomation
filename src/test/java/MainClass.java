20b574ca8960db0002c1b33473fe6a8dc3af6faf
IrinaVorontsova <irina.n.vorontsova@gmail.com>
2024-06-12 15:50:15 +0300

    homework for mobile authomator part 2

--- src/test/java/MainClass.java
+++ src/test/java/MainClass.java
@@ -1,7 +1,7 @@
 public class MainClass {
-    int number = 14;
+    private int class_number = 20;
 
-    public int getLocalNumber(){
-        return this.number;
+    public int getClassNumber(){
+        return this.class_number;
     }
 }
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -3,12 +3,12 @@ import org.junit.Test;
 
 public class MainClassTest {
     private MainClass mainClass = new MainClass();
-    int number = 14;
+    int number = 45;
 
     @Test
     public void testGetLocalNumber(){
-        Assert.assertTrue(String.format("%d != %d", number, mainClass.getLocalNumber()),
-                number == mainClass.getLocalNumber());
+        Assert.assertTrue(String.format("%d > %d", number, mainClass.getClassNumber()),
+                number < mainClass.getClassNumber());
 
     }
 }
